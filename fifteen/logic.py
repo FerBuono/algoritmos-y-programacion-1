b024a001ffd2707f15bd33ff1a4cda68fc3babe8
Fernando <fbuono@fi.uba.ar>
2022-04-24 21:54:13 -0300

    movimientos limitados y funciones optimizadas

--- fifteen/logic.py
+++ fifteen/logic.py
@@ -1,24 +1,52 @@
 import random
 
+VACIO = ""
+MOVIMIENTOS = ['a', 'w', 'd', 's']
+
+
 def formatear_dimensiones(dimensiones):
     '''
-        Funcion que recibe las dimensiones del juego en formato numxnum y devuelve ambos números por separado. Si el formato de ingreso no es el indicado entonces pide que se ingresen nuevamente las dimensiones
+        Funcion que recibe las dimensiones del juego en formato numxnum y 
+        devuelve ambos números por separado. Si el formato de ingreso no 
+        es el indicado entonces pide que se ingresen nuevamente las dimensiones
     '''
-    if 'x' not in dimensiones:
-        return formatear_dimensiones(input('Ingrese dimensiones válida (ej: 4x3): '))
+    dimensiones_validas = False
+    dimensiones_separadas = []
 
-    dimensiones_separadas = (dimensiones.split('x'))
+    while dimensiones_validas == False:
 
-    for dimension in dimensiones_separadas:
-        if dimension.isdigit() == False:
-            return formatear_dimensiones(input('Ingrese dimensiones válida (ej: 4x3): '))
+        if 'x' not in dimensiones:
+            dimensiones = input('Ingrese dimensiones válida (ej: 4x3): ')
+            continue
+
+        dimensiones_separadas = (dimensiones.split('x'))
+
+        if not dimensiones_separadas[0].isdigit() or not dimensiones_separadas[1].isdigit():
+            dimensiones = input('Ingrese dimensiones válida (ej: 4x3): ')
+            continue
+    
+        dimensiones_validas = True
 
     return int(dimensiones_separadas[0]), int(dimensiones_separadas[1])
         
-
-def crear_matriz(N, M):
+def randomizar_matriz(matriz, movimientos_random):
     '''
-        Función que recibe dos números enteros y devuelve una matriz de N filas x M columnas rellenada con números del 1 al (N * M - 1)
+        Función que altera el órden de los elementos de una matriz.
+        Utiliza el módulo random para generar números al azar entre
+        0 y la cantidad de movimientos posibles menos 1, y llama a 
+        la función mover() mandando como parámetros el movimiento 
+        dado por el número random, y la matriz a alterar
+    '''
+    for i in range(movimientos_random):
+        random_num = random.randint(0, len(MOVIMIENTOS) - 1)
+        mover(MOVIMIENTOS[random_num], matriz)
+    
+    return matriz
+
+def crear_matriz(N, M, movimientos_random):
+    '''
+        Función que recibe dos números enteros y devuelve una matriz 
+        de N filas x M columnas rellenada con números del 1 al (N * M - 1)
     '''
     num = 1
     matriz = []
@@ -31,59 +59,85 @@ def crear_matriz(N, M):
 
         num = 1 + matriz[fil][-1]
    
-    matriz[-1][-1] = ""
+    matriz[-1][-1] = VACIO
    
-    return matriz
+    return randomizar_matriz(matriz, movimientos_random)
 
 
-def mostrar_juego(matriz):
+def mostrar_juego(matriz, movimientos, limite_movimientos):
     '''
-        Función que recibe una matriz y la imprime con el formato indicado, junto con las instrucciones de juego
+        Función que recibe una matriz y la imprime con el formato indicado,
+        junto con las instrucciones de juego y los movimientos
     '''
     print("\n====", "Fiften", "====")
-    # for fil in matriz:
-    #     for col in fil:
-    #         if(col == fil[0]):
-    #             print(str(col).rjust(3), ' |', end='')
-    #         elif(col == fil[-1]):
-    #             print('  | ', str(col).ljust(3), '\n')
-    #         else:
-    #             print(str(col).rjust(3).ljust(3), end='')
     print('\n'.join(['  |'.join(['{:3}'.format(num) for num in fil]) for fil in matriz]))
     print("\nControles: w, a, s, d")
     print("Salir del juego: o")
-
+    print('Movimientos realizados: ', movimientos)
+    print('Movimientos restantes: ', limite_movimientos - movimientos)
 
 def mover(direccion, matriz):
     '''
-        Función que recibe una direccion (ya verificada que es válida) y la matriz a alterar, y devuelve la misma pero con las posiciones cambiadas según el movimiento a realizar. Direcciones válidas: 'a', 'w', 's, 'd'
+        Función que recibe una direccion (ya verificada que es válida) 
+        y la matriz a alterar, y devuelve la misma pero con las posiciones
+        cambiadas según el movimiento a realizar. 
+        Direcciones válidas: 'a', 'w', 's, 'd'
+    '''
+    fil_vacio, col_vacio = ubicar_vacio(matriz)
+    fil_nuevo, col_nuevo = fil_vacio, col_vacio
+
+    if direccion == 'w':
+        fil_nuevo = fil_vacio + 1
+    if direccion == 's':
+        fil_nuevo = fil_vacio - 1
+    if direccion == 'a':
+        col_nuevo = col_vacio + 1
+    if direccion == 'd':
+        col_nuevo = col_vacio - 1
+
+    if (0 <= fil_nuevo <= len(matriz) - 1) and (0 <= col_nuevo <= len(matriz[fil_nuevo]) - 1):
+        matriz[fil_vacio][col_vacio] = matriz[fil_nuevo][col_nuevo]
+        matriz[fil_nuevo][col_nuevo] = VACIO
+
+def ubicar_vacio(matriz):
+    '''
+        Función que ubica el casillero vacío en una matriz
     '''
     for fil in range(len(matriz)):
         for col in range(len(matriz[fil])):
-            if matriz[fil][col] == "":
-                if direccion == 'w' and fil != (len(matriz) - 1):
-                    matriz[fil][col] = matriz[fil + 1][col]
-                    matriz[fil + 1][col] = ""
-                    return
-                if direccion == 's' and fil != 0:
-                    matriz[fil][col] = matriz[fil - 1][col]
-                    matriz[fil - 1][col] = ""
-                    return
-                if direccion == 'a' and col != (len(matriz[fil]) - 1):
-                    matriz[fil][col] = matriz[fil][col + 1]
-                    matriz[fil][col + 1] = ""
-                    return
-                if direccion == 'd' and col != 0:
-                    matriz[fil][col] = matriz[fil][col - 1]
-                    matriz[fil][col - 1] = ""
-                    return
+            if matriz[fil][col] == VACIO:
+                return fil, col
 
 
-def randomizar_matriz(matriz, Z):
+def juego(matriz_juego, matriz_backup, movimientos, limite_movimientos):
     '''
-        Función que altera el órden de los elementos de una matriz. Utiliza el módulo random para generar números al azar entre 0 y la cantidad de movimientos posibles menos 1, y llama a la función mover() mandando como parámetros el movimiento dado por el número random, y la matriz a alterar
+        Funcion que verifica que mientras la matriz de juego sea distinta 
+        a la de backup y el jugador quiera seguir jugando, se le pida un 
+        nuevo movimiento y se matiene el estado del juego como 'Jugando'. 
+        El movimiento tiene que ser valido y de no ser así se le solicita 
+        una nueva entrada. Se muestra en cada entrada válida el tablero nuevamente. 
+        En caso de perder, finalizar el juego o ganar, cambia el estado del juego a:
+        'Perdiste', 'Juego finalizado' o 'Ganaste', respectivamente
     '''
-    movimientos = ['a', 'w', 'd', 's']
-    for i in range(Z):
-        random_num = random.randint(0, len(movimientos) - 1)
-        mover(movimientos[random_num], matriz)
+    while (matriz_juego != matriz_backup) and (movimientos < limite_movimientos):
+        entradas = input('Entrada/s: ')
+
+        for entrada in entradas:
+            while entrada not in MOVIMIENTOS + ['o']:
+                entrada = input('"' + entrada + '" no es válida, intente nuevamente: ')
+
+            if entrada == 'o':
+                mostrar_juego(matriz_juego, movimientos, limite_movimientos)
+                return 'Juego finalizado'
+            
+            mover(entrada, matriz_juego)
+            movimientos += 1
+
+            if movimientos >= limite_movimientos:
+                mostrar_juego(matriz_juego, movimientos, limite_movimientos)
+                return 'Perdiste'
+
+        mostrar_juego(matriz_juego, movimientos, limite_movimientos)
+    
+    return 'Ganaste'
+    
--- fifteen/main.py
+++ fifteen/main.py
@@ -1,65 +1,50 @@
-from re import M
-from logic import formatear_dimensiones, crear_matriz, mostrar_juego, mover, randomizar_matriz
+from logic import formatear_dimensiones, crear_matriz, mostrar_juego, juego
+
+MOVIMIENTOS_RANDOM = 50
 
 def main():
     '''
-        FIFTEEN: Es un sencillo juego donde el objetivo es ordenar todos los números de izquierda a derecha y de arriba a abajo dado un tablero de dimensiones NxM el cual siempre tiene un casillero vacío. Para ello el jugador puede mover las fichas en diferentes direcciones, usando siempre el espacio vacío.
+        FIFTEEN: Es un sencillo juego donde el objetivo es ordenar todos los números 
+        de izquierda a derecha y de arriba a abajo dado un tablero de dimensiones NxM 
+        el cual siempre tiene un casillero vacío. Para ello el jugador puede mover las 
+        fichas en diferentes direcciones, usando siempre el espacio vacío.
 
-        Finalmente, el juego se considera ganado si todos los elementos están ordenados de izquierda a derecha, con el vacío ubicado en el vértice inferior derecho.
+        Finalmente, el juego se considera ganado si todos los elementos están ordenados
+         de izquierda a derecha, con el vacío ubicado en el vértice inferior derecho.
 
         Direcciones válidas: 'a', 'w', 's, 'd'
         Para finalizar el juego: 'o'
     '''
-    # Variable en donde se guardan los movimientos realizados en la partida
     movimientos = 0
+  
+    limite_movimientos = MOVIMIENTOS_RANDOM * 5
 
-    # Ingreso las dimensiones del juego
     dimension_tablero = input('\nIngrese las dimensiones del tablero (ej: 4x3): ')
 
-    # Dimensiones separadas en filas y columnas
     filas, columnas = formatear_dimensiones(dimension_tablero)
 
-    # Creación de 2 matrices, con una se juega y la otra se guarda para el chequeo final
-    matriz_juego = crear_matriz(filas, columnas)
-    matriz_backup = crear_matriz(filas, columnas)
+    matriz_juego = crear_matriz(filas, columnas, MOVIMIENTOS_RANDOM)
+    # Matriz backup con la que comparar si gana o no
+    matriz_backup = crear_matriz(filas, columnas, 0)
 
-    # Cantidad de movimientos para 'randomizar' la matriz, y cantidad límite de movimientos
-    Z = (filas * columnas) * 5
-    N = Z * 5
+    # Mostrar juego inicial
+    mostrar_juego(matriz_juego, movimientos, limite_movimientos)
 
-    # 'Randomizado' de la matriz de juego 
-    randomizar_matriz(matriz_juego, Z)
-    
-    # Mostrar tablero inicial
-    mostrar_juego(matriz_juego)
-    print('Movimientos realizados: ', movimientos)
-    print('Movimientos restantes: ', N - movimientos)
+    estado_de_juego = 'Jugando'
 
-    # Verifico que mientras la matriz de juego sea distinta a la de backup y el jugador quiera seguir jugando, se le pida un nuevo movimiento. Este tiene que ser valido y de no ser así se le solicita una nueva entrada. Se muestra en cada entrada válida el tablero nuevamente
-    while (matriz_juego != matriz_backup) and (movimientos < N):
-        entradas = input('Entrada/s: ')
-        entradas_separadas = list(entradas)
-        for entrada in entradas_separadas:
-            while entrada not in ['a', 'w', 'd', 's', 'o']:
-                entrada = input('"' + entrada + '" no es válida, intente nuevamente: ')
-            if entrada == 'o':
-                mostrar_juego(matriz_juego)
-                print('Movimientos realizados: ', movimientos)
-                print('Movimientos restantes: ', N - movimientos)
-                print('\nJuego finalizado :(\n')
-                return
-            
-            mover(entrada, matriz_juego)
-            movimientos += 1
+    # Si el estado es 'Jugando', se sigue jugando
+    while estado_de_juego == 'Jugando':
+        estado_de_juego = juego(matriz_juego, matriz_backup, movimientos, limite_movimientos)
 
-        mostrar_juego(matriz_juego)
-        print('Movimientos realizados: ', movimientos)
-        print('Movimientos restantes: ', N - movimientos)
-    
-    if movimientos >= N:
+    if estado_de_juego == 'Ganaste':
+        print('\n¡GANASTE!\n')
+        return
+    elif estado_de_juego == 'Perdiste':
         print('\nPerdiste :(\n')
         return
+    else:
+        print('\nJuego finalizado :(\n')
+        return
 
-    print('\n¡GANASTE!\n')
 
 main()
