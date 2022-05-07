69bf7e133b07680629af6a613ec6573bb5d96a33
Fernando Buono <fbuono@fi.uba.ar>
2022-05-07 20:38:52 -0300

    entrega final del fifteen

--- fifteen/logic.py
+++ fifteen/logic.py
@@ -2,7 +2,7 @@ import random
 
 VACIO = ""
 MOVIMIENTOS = ['a', 'w', 'd', 's']
-
+MOVIMIENTOS_RANDOM = 50
 
 def formatear_dimensiones(dimensiones):
     '''
@@ -10,58 +10,54 @@ def formatear_dimensiones(dimensiones):
         devuelve ambos números por separado. Si el formato de ingreso no 
         es el indicado entonces pide que se ingresen nuevamente las dimensiones
     '''
-    dimensiones_validas = False
     dimensiones_separadas = []
 
-    while dimensiones_validas == False:
+    while True:
 
-        if 'x' not in dimensiones:
-            dimensiones = input('Ingrese dimensiones válida (ej: 4x3): ')
-            continue
+        if 'x' in dimensiones:
+            dimension_1, dimension_2 = dimensiones.split('x')
 
-        dimensiones_separadas = (dimensiones.split('x'))
-
-        if not dimensiones_separadas[0].isdigit() or not dimensiones_separadas[1].isdigit():
-            dimensiones = input('Ingrese dimensiones válida (ej: 4x3): ')
-            continue
-    
-        dimensiones_validas = True
-
-    return int(dimensiones_separadas[0]), int(dimensiones_separadas[1])
+            if dimension_1.isdigit() and dimension_2.isdigit():
+                break
         
-def randomizar_matriz(matriz, movimientos_random):
-    '''
-        Función que altera el órden de los elementos de una matriz.
-        Utiliza el módulo random para generar números al azar entre
-        0 y la cantidad de movimientos posibles menos 1, y llama a 
-        la función mover() mandando como parámetros el movimiento 
-        dado por el número random, y la matriz a alterar
-    '''
-    for i in range(movimientos_random):
-        random_num = random.randint(0, len(MOVIMIENTOS) - 1)
-        mover(MOVIMIENTOS[random_num], matriz)
-    
-    return matriz
+        dimensiones = input('Ingrese dimensiones válidas (ej: 4x3): ')
+        continue
 
-def crear_matriz(N, M, movimientos_random):
+    return int(dimension_1), int(dimension_2)
+
+
+def crear_matriz(dimension_tablero):
     '''
         Función que recibe dos números enteros y devuelve una matriz 
-        de N filas x M columnas rellenada con números del 1 al (N * M - 1)
+        desordenada de N filas x M columnas rellenada con números del 
+        1 al (N * M - 1). Para desordenarla se utiliza random.choice y
+        llama a la función mover(), mandando como parámetro un movimiento
+        al azar y la matriz
     '''
+    N, M = formatear_dimensiones(dimension_tablero)
+
     num = 1
     matriz = []
+    matriz_backup = []
 
     for fil in range(N):
         matriz.append([])
+        matriz_backup.append([])
 
         for col in range(M):
             matriz[fil].append(num + col)
+            matriz_backup[fil].append(num + col)
 
         num = 1 + matriz[fil][-1]
+        num = 1 + matriz_backup[fil][-1]
    
     matriz[-1][-1] = VACIO
+    matriz_backup[-1][-1] = VACIO
    
-    return randomizar_matriz(matriz, movimientos_random)
+    for i in range(MOVIMIENTOS_RANDOM):
+        mover(random.choice(MOVIMIENTOS), matriz)
+
+    return matriz, matriz_backup
 
 
 def mostrar_juego(matriz, movimientos, limite_movimientos):
@@ -75,6 +71,7 @@ def mostrar_juego(matriz, movimientos, limite_movimientos):
     print("Salir del juego: o")
     print('Movimientos realizados: ', movimientos)
     print('Movimientos restantes: ', limite_movimientos - movimientos)
+
 
 def mover(direccion, matriz):
     '''
@@ -99,6 +96,7 @@ def mover(direccion, matriz):
         matriz[fil_vacio][col_vacio] = matriz[fil_nuevo][col_nuevo]
         matriz[fil_nuevo][col_nuevo] = VACIO
 
+
 def ubicar_vacio(matriz):
     '''
         Función que ubica el casillero vacío en una matriz
@@ -108,36 +106,3 @@ def ubicar_vacio(matriz):
             if matriz[fil][col] == VACIO:
                 return fil, col
 
-
-def juego(matriz_juego, matriz_backup, movimientos, limite_movimientos):
-    '''
-        Funcion que verifica que mientras la matriz de juego sea distinta 
-        a la de backup y el jugador quiera seguir jugando, se le pida un 
-        nuevo movimiento y se matiene el estado del juego como 'Jugando'. 
-        El movimiento tiene que ser valido y de no ser así se le solicita 
-        una nueva entrada. Se muestra en cada entrada válida el tablero nuevamente. 
-        En caso de perder, finalizar el juego o ganar, cambia el estado del juego a:
-        'Perdiste', 'Juego finalizado' o 'Ganaste', respectivamente
-    '''
-    while (matriz_juego != matriz_backup) and (movimientos < limite_movimientos):
-        entradas = input('Entrada/s: ')
-
-        for entrada in entradas:
-            while entrada not in MOVIMIENTOS + ['o']:
-                entrada = input('"' + entrada + '" no es válida, intente nuevamente: ')
-
-            if entrada == 'o':
-                mostrar_juego(matriz_juego, movimientos, limite_movimientos)
-                return 'Juego finalizado'
-            
-            mover(entrada, matriz_juego)
-            movimientos += 1
-
-            if movimientos >= limite_movimientos:
-                mostrar_juego(matriz_juego, movimientos, limite_movimientos)
-                return 'Perdiste'
-
-        mostrar_juego(matriz_juego, movimientos, limite_movimientos)
-    
-    return 'Ganaste'
-    
